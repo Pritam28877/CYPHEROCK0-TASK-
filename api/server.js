c921458e4b05ed4c2d224cc64392bfdb74fbf370
Pritam Das <dpritam28577@gmail.com>
2023-11-27 13:09:31 +0530

    Update server.js file for database connection and port configuration

--- api/server.js
+++ api/server.js
@@ -1,10 +1,26 @@
-const experss = require("express");
-const authController = require("../Controllers/authController");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-const router = experss.Router();
+dotenv.config({ path: "./config.env" });
 
+const app = require("./app");
 
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
 
+// const DB = process.env.DATABASE_LOCAL;
 
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB connection successful!"));
 
+const port = process.env.PORT || 3000;
 
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
