df714ec56512fb9b81bb5ad37faebe7186d2a5bd
Pritam Das <dpritam28577@gmail.com>
2023-11-27 12:54:52 +0530

    Add restrictTo middleware and getAllToken endpoint

--- api/Api/Controllers/authController.js
+++ api/Api/Controllers/authController.js
@@ -97,3 +97,32 @@ exports.protect = async (req,res,next)=>{
         })
     }
 }
+
+exports.restrictTo = (...roles)=>{
+    return (req,res,next)=>{
+        if(!roles.includes(req.user.role)){
+            res.status(403).json({
+                status:'fail',
+                message:'You do not have permission to perform this action'
+            })
+        }
+        next();
+    }
+}
+
+exports.getAllToken = async (req,res,next)=>{
+    try{
+        const token = await Token.find();
+        res.status(200).json({
+            status:'success',
+            data:{
+                token
+            }
+        })
+    }catch(err){
+        res.status(400).json({
+            status:'fail',
+            message:err
+        })
+    }
+}
